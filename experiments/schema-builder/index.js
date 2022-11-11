984182221458deb5b261eae40c539933cbaaa8e9
Jalinson Diaz <zjaaaldev@gmail.com>
2022-11-11 15:47:56 -0300

    found out that proxy is unnecessary

--- experiments/schema-builder/index.js
+++ experiments/schema-builder/index.js
@@ -3,22 +3,11 @@ import number from "./number.js";
 import boolean from "./boolean.js";
 import object from "./object.js";
 
-const handler = {
-  apply: (target, thisArgs, args) => {
-    return new target(...args);
-  },
-};
-
-const stringProxy = new Proxy(string, handler);
-const numberProxy = new Proxy(number, handler);
-const booleanProxy = new Proxy(boolean, handler);
-const objectProxy = new Proxy(object, handler);
-
 const schemaBuilder = {
-  string: stringProxy,
-  number: numberProxy,
-  boolean: booleanProxy,
-  object: objectProxy,
+  string: () => new string(),
+  number: () => new number(),
+  boolean: () => new boolean(),
+  object: (schema) => new object(schema),
 };
 
 export default schemaBuilder;
