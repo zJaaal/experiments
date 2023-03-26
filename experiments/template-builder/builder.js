933d949a8bb45018f912cb1e173001058ea2f3c5
Jalinson Diaz <zjaaaldev@gmail.com>
2023-03-25 23:00:36 -0300

    code cleaning and add quantity to content

--- experiments/template-builder/builder.js
+++ experiments/template-builder/builder.js
@@ -8,6 +8,11 @@ export class TemplateBuilder {
 
   _selfClosingFlag = false;
 
+  /**
+   * @description Initialize the builder with a tag
+   * @param {string} tag string representing the tag
+   * @returns {TemplateBuilder} builder
+   */
   createTag(tag) {
     this._tag = `<${tag}`;
     this._selfClosingFlag = selfClosing[tag];
@@ -17,20 +22,42 @@ export class TemplateBuilder {
     return this;
   }
 
+  /**
+   * @description Adds a property to the tag
+   * @param {string} prop Key of the property
+   * @param {string} value Value of the property
+   * @returns {TemplateBuilder} builder
+   */
   addProp(prop, value) {
+    if (!this._tag.length) throw new Error("Please initialize the builder using createTag method");
+
     this._props.push(`${prop}="${value}"`);
     return this;
   }
 
-  addContent(content) {
+  /**
+   * @description Adds inner content to the tag if is not a self closing tag
+   * @param {string} content String representing the content
+   * @param {number} quantity Number of times it repeats
+   * @returns {TemplateBuilder} builder
+   */
+  addContent(content, quantity = 1) {
+    if (!this._tag.length) throw new Error("Please initialize the builder using createTag method");
+
     this._selfClosingFlag
       ? console.warn("Cannot add content to a self closing tag")
-      : (this._content += content);
+      : (this._content += content.repeat(quantity));
 
     return this;
   }
 
-  create() {
+  /**
+   * @description Builds the template using the tag, props and content provided
+   * @returns {string} template
+   */
+  build() {
+    if (!this._tag.length) throw new Error("Please initialize the builder using createTag method");
+
     return this._selfClosingFlag
       ? `${this._tag} ${this._props.join(" ")} ${this._closing}`
       : `${this._tag} ${this._props.join(" ")}>${this._content}${this._closing}`;
